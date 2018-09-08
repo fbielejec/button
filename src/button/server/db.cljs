330fff7e2ef5ba58a458294e7fba74052ed8b3ca
filip <fbielejec@gmail.com>
2018-09-08 11:42:48 +0200

    reoslvers

--- src/button/server/db.cljs
+++ src/button/server/db.cljs
@@ -1,5 +1,47 @@
 (ns button.server.db
-  (:require [mount.core :as mount]))
+  #_(:require [district.server.config :refer [config]]
+              [district.server.db :as db]
+              [district.server.db.column-types :refer [address not-nil default-nil default-zero default-false sha3-hash primary-key]]
+              [district.server.db.honeysql-extensions]
+              [honeysql.core :as sql]
+              [honeysql.helpers :refer [merge-where merge-order-by merge-left-join defhelper]]
+              [medley.core :as medley]
+              [mount.core :as mount :refer [defstate]]
+              [print.foo :refer [look] :include-macros true]
+              [taoensso.timbre :as logging :refer-macros [info warn error]]))
 
-(defstate db :start (atom {})
-  :stop ::stopped)
+#_(def ipfs-hash (sql/call :char (sql/inline 46)))
+
+#_(def registry-entries-columns
+    [[:button-token/token-id address primary-key not-nil]
+     [:button-token/number :unsigned :integer not-nil]
+     [:button-token/owner-address address not-nil]
+     [:button-token/we :unsigned :BIG :INT not-nil]
+     [:reg-entry/created-on :unsigned :integer not-nil]
+     [:reg-entry/challenge-period-end :unsigned :integer not-nil]
+     [:challenge/challenger address default-nil]
+     [:challenge/created-on :unsigned :integer default-nil]
+     [:challenge/voting-token address default-nil]
+     [:challenge/reward-pool :unsigned :BIG :INT default-nil]
+     [:challenge/meta-hash ipfs-hash default-nil]
+     [:challenge/comment :varchar default-nil]
+     [:challenge/commit-period-end :unsigned :integer default-nil]
+     [:challenge/reveal-period-end :unsigned :integer default-nil]
+     [:challenge/votes-for :BIG :INT default-nil]
+     [:challenge/votes-against :BIG :INT default-nil]
+     [:challenge/claimed-reward-on :unsigned :integer default-nil]])
+
+(defn start [args]
+  (atom {:last-block 0
+         :tokens []}))
+
+(defn stop []
+  ::stopped)
+
+(defstate ^{:on-reload :noop} button-db
+  :start (start (merge (:button/db @config)
+                       (:button/db (mount/args))))
+  :stop (stop))
+
+(defn get-all-tokens []
+  (:tokens @@button-db))
---
+++ src/button/server/graphql_resolvers.cljs
@@ -0,0 +1,26 @@
+(ns button.server.graphql-resolvers
+  (:require [bignumber.core :as bn]
+            ;; [cljs-time.core :as t]
+            ;; [cljs-web3.core :as web3-core]
+            ;; [cljs-web3.eth :as web3-eth]
+            [cljs.nodejs :as nodejs]
+            [clojure.string :as str]
+
+            [district.graphql-utils :as graphql-utils]
+            ;; [district.server.config :refer [config]]
+            ;; [district.server.db :as db]
+            ;; [district.server.smart-contracts :as smart-contracts]
+            [district.server.web3 :as web3]
+            ;; [honeysql.core :as sql]
+            ;; [honeysql.helpers :as sqlh]
+            [print.foo :refer [look] :include-macros true]
+            [taoensso.timbre :as log]
+            [button.server.db :as button-db]))
+
+(def enum graphql-utils/kw->gql-name)
+
+(defn all-tokens-query-resolver [_ _]
+  (button-db/get-all-tokens))
+
+(def resolvers-map
+  {:Query {:meme all-tokens-query-resolver}})
