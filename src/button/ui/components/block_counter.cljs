4fd8f01cb93d2b2981b5a14367e36819af72ecf2
Daniel Glauser <daniel@cambium.consulting>
2018-09-08 22:56:38 +0200

    Keine negative numbers

--- src/button/ui/components/block_counter.cljs
+++ src/button/ui/components/block_counter.cljs
@@ -5,10 +5,9 @@
 (defn component []
   (fn [data]
     (let [last-pressed-block-number (:last-press-block-number data)
-          current-block-number @(re-frame/subscribe [::subs/current-block-number])]
-      (js/console.log "lpbn:" last-pressed-block-number)
-      (js/console.log "current block number:" current-block-number)
+          current-block-number @(re-frame/subscribe [::subs/current-block-number])
+          absolute-difference (max 0 (- current-block-number
+                                        last-pressed-block-number))]
       [:div.block-counter
        [:span
-        (- current-block-number
-           last-pressed-block-number)]])))
+        absolute-difference]])))
